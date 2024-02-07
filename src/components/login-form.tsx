1462566276903a391ff856e4f1d201eb61304635
Rickard Lindahl <rickard@lindahl.app>
2024-02-07 11:04:38 +0100

    Update login-form to use react-hook-form & zod

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -1,30 +1,55 @@
+import { type SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+
+const loginFormSchema = z.object({
+    username: z.string().min(1, { message: 'A username is required' }),
+    password: z.string().min(1, { message: 'A password is required' }),
+});
+
+type LoginForm = z.infer<typeof loginFormSchema>;
+
 export function LoginForm() {
-	return (
-		<div className="login" style={{ display: "block" }}>
-			<div className="ui grid centered">
-				<form>
-					<div className="fields">
-						<div className="required field">
-							<div className="ui icon input">
-								<input type="text" name="username" placeholder="Username" />
-								<i className="user icon" />
-							</div>
-						</div>
-						<div className="required field">
-							<div className="ui icon input">
-								<input type="password" name="password" placeholder="Password" />
-								<i className="lock icon" />
-							</div>
-						</div>
-						<div className="field">
-							<div className="ui icon input">
-								<input type="submit" value="Login" />
-								<i className="right chevron icon" />
-							</div>
-						</div>
-					</div>
-				</form>
-			</div>
-		</div>
-	);
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<LoginForm>({
+        resolver: zodResolver(loginFormSchema),
+    });
+
+    const onSubmit: SubmitHandler<LoginForm> = (data) => {
+        console.log("onSubmit", data);
+    };
+
+    return (
+        <div className="login" style={{ display: "block" }}>
+            <div className="ui grid centered">
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="fields">
+                        <div className="required field">
+                            <div className="ui icon input">
+                                <input type="text" id="username" placeholder="Username" {...register("username", { required: true })} />
+                                <i className="user icon" />
+                            </div>
+                        </div>
+                        <div className="required field">
+                            <div className="ui icon input">
+                                <input type="password" id="password" placeholder="Password" {...register("password", { required: true })} />
+                                <i className="lock icon" />
+                            </div>
+                        </div>
+                        <div className="field">
+                            <div className="ui icon input">
+                                <input type="submit" value="Login" />
+                                <i className="right chevron icon" />
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            {errors.username && <p>{errors.username.message}</p>}
+            {errors.password && <p>{errors.password.message}</p>}
+        </div>
+    );
 }

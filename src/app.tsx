e788c8b3c38e292b0173da208baec3f9d4339430
Rickard Lindahl <rickard@lindahl.app>
2024-02-07 13:03:06 +0100

    Call API without handling result onSubmit
    meh

--- src/app.tsx
+++ src/app.tsx
@@ -9,17 +9,17 @@ const router = createRouter({ routeTree });
 
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
-    interface Register {
-        router: typeof router;
-    }
+	interface Register {
+		router: typeof router;
+	}
 }
 
 const queryClient = new QueryClient();
 
 export function App() {
-    return (
-        <QueryClientProvider client={queryClient}>
-            <RouterProvider router={router} />
-        </QueryClientProvider>
-    )
+	return (
+		<QueryClientProvider client={queryClient}>
+			<RouterProvider router={router} />
+		</QueryClientProvider>
+	);
 }
--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -3,53 +3,80 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 
 const loginFormSchema = z.object({
-    username: z.string().min(1, { message: 'A username is required' }),
-    password: z.string().min(1, { message: 'A password is required' }),
+	username: z.string().min(1, { message: "A username is required" }),
+	password: z.string().min(1, { message: "A password is required" }),
 });
 
 type LoginForm = z.infer<typeof loginFormSchema>;
 
+async function fetchLogin(credentials: LoginForm) {
+	const res = await fetch("http://localhost:3001/login", {
+		method: "POST",
+		body: JSON.stringify(credentials),
+		headers: {
+			Accept: "application/json",
+			"Content-Type": "application/json",
+		},
+	});
+	return res.json();
+}
+
 export function LoginForm() {
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm<LoginForm>({
-        resolver: zodResolver(loginFormSchema),
-    });
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<LoginForm>({
+		resolver: zodResolver(loginFormSchema),
+	});
 
-    const onSubmit: SubmitHandler<LoginForm> = (data) => {
-        console.log("onSubmit", data);
-    };
+	const onSubmit: SubmitHandler<LoginForm> = async (data) => {
+		try {
+			const result = await fetchLogin(data);
+			console.log(result);
+		} catch (e) {
+			console.error(e);
+		}
+	};
 
-    return (
-        <div className="login" style={{ display: "block" }}>
-            <div className="ui grid centered">
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <div className="fields">
-                        <div className="required field">
-                            <div className="ui icon input">
-                                <input type="text" id="username" placeholder="Username" {...register("username", { required: true })} />
-                                <i className="user icon" />
-                            </div>
-                        </div>
-                        <div className="required field">
-                            <div className="ui icon input">
-                                <input type="password" id="password" placeholder="Password" {...register("password", { required: true })} />
-                                <i className="lock icon" />
-                            </div>
-                        </div>
-                        <div className="field">
-                            <div className="ui icon input">
-                                <input type="submit" value="Login" />
-                                <i className="right chevron icon" />
-                            </div>
-                        </div>
-                    </div>
-                </form>
-            </div>
-            {errors.username && <p>{errors.username.message}</p>}
-            {errors.password && <p>{errors.password.message}</p>}
-        </div>
-    );
+	return (
+		<div className="login" style={{ display: "block" }}>
+			<div className="ui grid centered">
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<div className="fields">
+						<div className="required field">
+							<div className="ui icon input">
+								<input
+									type="text"
+									id="username"
+									placeholder="Username"
+									{...register("username", { required: true })}
+								/>
+								<i className="user icon" />
+							</div>
+						</div>
+						<div className="required field">
+							<div className="ui icon input">
+								<input
+									type="password"
+									id="password"
+									placeholder="Password"
+									{...register("password", { required: true })}
+								/>
+								<i className="lock icon" />
+							</div>
+						</div>
+						<div className="field">
+							<div className="ui icon input">
+								<input type="submit" value="Login" />
+								<i className="right chevron icon" />
+							</div>
+						</div>
+					</div>
+				</form>
+			</div>
+			{errors.username && <p>{errors.username.message}</p>}
+			{errors.password && <p>{errors.password.message}</p>}
+		</div>
+	);
 }

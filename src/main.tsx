fe053d9a7a64b3a80fb65cf049a5aa09c88a78f2
Rickard Lindahl <rickard@lindahl.app>
2024-02-07 11:10:29 +0100

    Move setup of router to app component

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createRouter } from "@tanstack/react-router";
-
-// Import the generated route tree
-import { routeTree } from "./routeTree.gen";
-
-// Create a new router instance
-const router = createRouter({ routeTree });
-
-// Register the router instance for type safety
-declare module "@tanstack/react-router" {
-	interface Register {
-		router: typeof router;
-	}
-}
+import { App } from "./app";
 
 // biome-ignore lint/style/noNonNullAssertion: We know the root element exists
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<RouterProvider router={router} />
+		<App />
 	</React.StrictMode>,
 );

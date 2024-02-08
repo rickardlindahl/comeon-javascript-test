c3f06f1b8bfd663cd692a0f822b5fb0a9604aa71
Rickard Lindahl <rickard@lindahl.app>
2024-02-08 22:46:46 +0100

    Add react-query (again)

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,5 @@
 import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
@@ -23,18 +24,22 @@ declare module "@tanstack/react-router" {
 	}
 }
 
+const queryClient = new QueryClient();
+
 export function App() {
 	const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
 
 	return (
-		<RouterProvider
-			router={router}
-			context={{
-				auth: {
-					isAuthenticated,
-				},
-				casinoApi: createCasinoApi(import.meta.env.VITE_API_URL),
-			}}
-		/>
+		<QueryClientProvider client={queryClient}>
+			<RouterProvider
+				router={router}
+				context={{
+					auth: {
+						isAuthenticated,
+					},
+					casinoApi: createCasinoApi(import.meta.env.VITE_API_URL),
+				}}
+			/>
+		</QueryClientProvider>
 	);
 }
